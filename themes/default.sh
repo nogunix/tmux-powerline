b048ce252d09b779775001cb4fe4d64001e0d998
Nogunix <nogunix@gmail.com>
2025-09-24 20:12:37 +0900

    feat: Apply new bubble theme with Catppuccin colors to default.sh

--- themes/default.sh
+++ themes/default.sh
@@ -1,6 +1,55 @@
-# shellcheck shell=bash
-# Default Theme
-# If changes made here does not take effect, then try to re-create the tmux session to force reload.
+# shellcheck shell=bash disable=SC2034
+####################################################################################################
+# This is a bubble theme created by @embe221ed (https://github.com/embe221ed)
+# colors are inspired by catppuccin palettes (https://github.com/catppuccin/catppuccin)
+####################################################################################################
+
+# COLORS
+
+# background for frappe catppuccin terminal theme
+# thm_bg="#303446"
+
+# background for macchiato catppuccin terminal theme
+thm_bg="#24273A"
+
+thm_fg="#c6d0f5"
+thm_cyan="#99d1db"
+thm_black="#292c3c"
+thm_gray="#414559"
+thm_magenta="#ca9ee6"
+thm_pink="#f4b8e4"
+thm_blue="#8caaee"
+thm_black4="#626880"
+rosewater="#f2d5cf"
+flamingo="#eebebe"
+pink="#f4b8e4"
+mauve="#ca9ee6"
+red="#e78284"
+maroon="#ea999c"
+peach="#ef9f76"
+yellow="#e5c890"
+green="#a6d189"
+teal="#81c8be"
+sky="#99d1db"
+sapphire="#85c1dc"
+blue="#8caaee"
+lavender="#babbf1"
+text="#c6d0f5"
+subtext1="#b5bfe2"
+subtext0="#a5adce"
+overlay2="#949cbb"
+overlay1="#838ba7"
+overlay0="#737994"
+surface2="#626880"
+surface1="#51576d"
+surface0="#414559"
+base="#303446"
+mantle="#292c3c"
+crust="#232634"
+eggplant="#e889d2"
+sky_blue="#a7c7e7"
+spotify_green="#1db954"
+spotify_black="#191414"
 
 if tp_patched_font_in_use; then
 	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
@@ -15,24 +64,24 @@ else
 fi
 
 # See Color formatting section below for details on what colors can be used here.
-TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'236'}
-TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'153'}
-# shellcheck disable=SC2034
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-$thm_bg}
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-$thm_fg}
 TMUX_POWERLINE_SEG_AIR_COLOR=$(tp_air_color)
 
 TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
 TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
 
 # See `man tmux` for additional formatting options for the status line.
-# The `format regular` and `format inverse` functions are provided as conveniences
+# The `format regular` and `format inverse` functions are provided as conveinences
 
 # shellcheck disable=SC2128
 if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_CURRENT" ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
+		"#[$(tp_format regular)]"
+		"$TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR"
 		"#[$(tp_format inverse)]"
-		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
 		" #I#F "
-		"$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
+		"$TMUX_POWERLINE_SEPARATOR_THIN"
 		" #W "
 		"#[$(tp_format regular)]"
 		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
@@ -51,7 +100,7 @@ if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
 		"#[$(tp_format regular)]"
 		"  #I#{?window_flags,#F, } "
-		"$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
+		"$TMUX_POWERLINE_SEPARATOR_THIN"
 		" #W "
 	)
 fi
@@ -59,7 +108,7 @@ fi
 # Format: segment_name [background_color|default_bg_color] [foreground_color|default_fg_color] [non_default_separator|default_separator] [separator_background_color|no_sep_bg_color]
 #                      [separator_foreground_color|no_sep_fg_color] [spacing_disable|no_spacing_disable] [separator_disable|no_separator_disable]
 #
-# * background_color and foreground_color. Color formatting (see `man tmux` for complete list) or run the color_palette.sh in the tmux-powerline root directory:
+# * background_color and foreground_color. Color formatting (see `man tmux` for complete list):
 #   * Named colors, e.g. black, red, green, yellow, blue, magenta, cyan, white
 #   * Hexadecimal RGB string e.g. #ffffff
 #   * 'default_fg_color|default_bg_color' for the default theme bg and fg color
@@ -77,13 +126,11 @@ fi
 #   * "left_disable" - disable space on the left
 #   * "right_disable" - disable space on the right
 #   * "both_disable" - disable spaces on both sides
-#   * - any other character/string produces no change to default behavior (eg "none", "X", etc.)
 #
 # * separator_disable - disables drawing a separator on this segment, very useful for segments
 #   with dynamic background colours (eg tmux_mem_cpu_load):
 #   * "no_separator_disable" - don't disable the separator (default)
 #   * "separator_disable" - disables the separator
-#   * - any other character/string produces no change to default behavior
 #
 # Example segment with separator disabled and right space character disabled:
 # "hostname 33 0 {TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD} 0 0 right_disable separator_disable"
@@ -96,21 +143,20 @@ fi
 #
 ## Note that although redundant the non_default_separator, separator_background_color and
 # separator_foreground_color options must still be specified so that appropriate index
-# of options to support the spacing_disable and separator_disable features can be used
+# of options to support the spacing_disable and separator_disable features can be used.
 # The default_* and no_* can be used to keep the default behaviour.
 
 # shellcheck disable=SC1143,SC2128
 if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 111 236"
-		"hostname 149 236"
-		#"mode_indicator 165 0"
+		"tmux_session_info $blue $thm_bg"
+		"hostname $eggplant $thm_bg"
 		#"ifstat 30 255"
 		#"ifstat_sys 30 255"
-		"lan_ip 117 236"
-		#"vpn 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
-		#"wan_ip 117 236"
-		"vcs_branch 141 236"
+		"lan_ip $sky_blue $thm_bg ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
+		"wan_ip $sky_blue $thm_bg"
+		"vcs_branch $thm_gray"
+		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} $thm_bg"
 		#"vcs_compare 60 255"
 		#"vcs_staged 64 255"
 		#"vcs_modified 9 255"
@@ -121,22 +167,21 @@ fi
 # shellcheck disable=SC1143,SC2128
 if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		#"earthquake 3 0"
-		"pwd 179 236"
+		# "earthquake 3 0"
+		"pwd $mauve $surface0"
 		#"macos_notification_count 29 255"
 		#"mailcount 9 255"
-		# "now_playing 234 37"
+		"now_playing $spotify_green $spotify_black"
 		#"cpu 240 136"
-		#"load 215 236"
-		#"battery 149 236"
-		#"weather 117 236"
+		#"load 237 167"
+		#"tmux_mem_cpu_load 234 136"
+		"battery $blue $thm_bg"
+		#"weather 37 255"
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}"
 		#"xkb_layout 125 117"
-		#"tmux_continuum_save"
-		#"tmux_continuum_status 14 7"
-		#"date_day 239 153"
-		#"date 239 153 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-		#"time 239 153 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"date_day $teal $thm_bg"
+		"date $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"time $teal $thm_bg ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 	)
 fi
